b01e814e3b773742f1310a325f983c1bc0b87036
HarisAli9 <160336041+HarisAli9@users.noreply.github.com>
2024-12-09 01:34:05 +0500

    Add files via upload

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from "./hero"
+
+const contact = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  )
+}
+
+export default contact
---
+++ src/app/faqpage/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from "./hero"
+import FAQPage from "./faqpage"
+
+const FAQpage = () => {
+  return (
+    <div>
+      <Hero />
+      <FAQPage />
+    </div>
+  )
+}
+
+export default FAQpage
